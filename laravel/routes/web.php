7712cd8196472fd7c8e059aa34c3884042eb1ef6
fadi <fadi.alset.94@gmail.com>
2025-01-13 13:37:40 +0100

    replcae welcome page with Dasbhoard

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Auth::check() ? redirect('/dashboard') : redirect('/login');
 });
 
 Route::get('/dashboard', function () {
