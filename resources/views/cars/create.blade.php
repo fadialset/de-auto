2b2403be3a77e470e8960767d31f86e1b545c57f
fadi <fadi.alset.94@gmail.com>
2025-01-20 14:36:25 +0100

    fix adding cars

--- resources/views/cars/create.blade.php
+++ resources/views/cars/create.blade.php
@@ -92,9 +92,10 @@
                     <div class="mb-4">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="transmission">Transmissie:</label>
                         <select id="transmission" name="transmission" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                            <option value="automatic" {{ old('transmission') == 'automatic' ? 'selected' : '' }}>Automatic</option>
-                            <option value="manual" {{ old('transmission') == 'manual' ? 'selected' : '' }}>Manual</option>
-                        </select>
+    <option value="">Select Transmission</option>
+    <option value="automatic">Automatisch</option>
+    <option value="manual">Handmatig</option>
+</select>
                     </div>
 
                     <div class="mb-4">
